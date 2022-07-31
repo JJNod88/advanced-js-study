04de31c1aa57d84066f7cbbd882171803cb52d84
JJNod88 <j.noddings@hotmail.com>
2022-07-31 14:10:39 +0000

    completed js advanced, useful js code here

--- arrow-functions.js
+++ arrow-functions.js
@@ -34,7 +34,7 @@ console.log(sum2)
 
 
 
-
+/** 
 let arr1 = [1, 2, 3];
 let arr2 = arr1;
 arr1.push(4)
@@ -94,3 +94,110 @@ const makeList = (arr, student) => {
 let [John] = makeList(1, 2)
 console.log(makeList)
 
+
+let nums = [1, 2, 3, 4];
+const results = nums.map(nums => nums * 2);
+console.log(results)
+
+const people = [
+    {
+        name: 'Michael',
+        age: 23,
+    },
+    {
+        name: 'Liana',
+        age: 16,
+    },
+    {
+        name: 'Ben',
+        age: 20,
+    },
+]
+
+const oldEnough = people.filter(people => people.age >= 21);
+console.log(oldEnough)
+
+const ben = people.filter(people => people.name == 'Ben')
+console.log(ben)
+
+let students = [
+    {
+        name: 'John',
+        subjects: ['maths', 'english', 'cad'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Emily',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 80, art: 95},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 84, maths: 97, art: 95},
+    },
+    {
+        name: 'Fran',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 67, english: 87, art: 95},
+    }
+];
+
+const topMaths = students.filter(itm => itm.results.maths >= 90);
+console.log(topMaths);
+
+
+const numbs = [1, 2, 3, 4];
+let sum = numbs.reduce((acc, curr) => acc + curr);
+console.log(sum)
+// acc = accumulator curr = current value
+
+*/
+
+const teamMembers = [
+    {
+        name: 'Andrew',
+        profession: 'Developer',
+        yrsExperience: 5
+    },
+    {
+        name: 'ben',
+        profession: 'Developer',
+        yrsExperience: 7
+    },
+    {
+        name: 'laura',
+        profession: 'designer',
+        yrsExperience: 2
+    },
+    {
+        name: 'heather',
+        profession: 'designer',
+        yrsExperience: 9
+    },
+];
+
+// totaling an object i.e yrs experience
+
+const totalXp = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalXp)
+
+// grouping properties i.e designer and developer
+
+let xpByProf = teamMembers.reduce((acc, curr) => acc.profession + curr.yrsExperience, 0);
+//console.log(xpByProf); // didnt work
+
+let xpByProf2 = teamMembers.reduce((acc, curr) => {
+    let key = curr.profession;
+    if (!acc[key]) {
+        acc[key] = curr.yrsExperience;
+    } else {
+        acc[key] += curr.yrsExperience;
+    }
+    return acc;
+}, {});
+console.log(xpByProf2);
